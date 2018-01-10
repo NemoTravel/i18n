22373bc2c92f3f9a65a845a594c92c33489b65a9
pnaumov <ecrofeg1@gmail.com>
2018-01-10 14:58:54 +0300

    Module

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const message = 'Hello World!';
+var storage = null;
+
+function i18n(key) {
+	if (!storage) {
+		throw new Error('Please specify localization storage with the `init` static method.');
+	}
+
+	return storage.hasOwnProperty(key) ? storage[key] : key;
+}
+
+i18n.init = function (object) {
+	storage = object;
+};
+
+module.exports = i18n;
